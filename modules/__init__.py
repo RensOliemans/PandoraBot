20b8f8b5670cce7348fae2c8cfa3fb67fb229bfb
Noah Goldsmid <noah.goldsmid@antagonist.nl>
2019-04-11 12:28:57 +0200

    Add OCR module

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,8 +1,9 @@
 from .iapandora.module import IAPandora
 from .puzzles.module import Puzzles
 from .location.module import Location
+from .photo.module import Photo
 
-loaded_modules = [IAPandora, Puzzles, Location]
+loaded_modules = [IAPandora, Puzzles, Location, Photo]
 
 from .help.module import Help
 
